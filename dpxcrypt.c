167ff2cf360a2e0f0696a29262f82d681cfc84f4
Shunsaku Hirata <shunsaku.hirata74@gmail.com>
2015-04-29 13:24:25 +0900

    SHA256

--- dpxcrypt.c
+++ dpxcrypt.c
@@ -44,6 +44,7 @@ static void _gcry_burn_stack (int bytes)
 
 /* Rotate a 32 bit integer by n bytes */
 #define rol(x,n) ( ((x) << (n)) | ((x) >> (32-(n))) )
+#define ror(x,n) ( ((x) >> ((n)&(32-1))) | ((x) << ((32-(n))&(32-1))) )
 
 /*
  * The following codes for MD5 Message-Digest Algorithm were modified
@@ -73,13 +74,13 @@ static void _gcry_burn_stack (int bytes)
 
 void MD5_init (MD5_CONTEXT *ctx)
 {
-  ctx->A = 0x67452301;
-  ctx->B = 0xefcdab89;
-  ctx->C = 0x98badcfe;
-  ctx->D = 0x10325476;
+  ctx->A = 0x67452301U;
+  ctx->B = 0xefcdab89U;
+  ctx->C = 0x98badcfeU;
+  ctx->D = 0x10325476U;
 
   ctx->nblocks = 0;
-  ctx->count = 0;
+  ctx->count   = 0;
 }
 
 /* These are the four functions used in the four steps of the MD5 algorithm
@@ -95,10 +96,10 @@ void MD5_init (MD5_CONTEXT *ctx)
 static void transform (MD5_CONTEXT *ctx, const unsigned char *data)
 {
   uint32_t correct_words[16];
-  register uint32_t A = ctx->A;
-  register uint32_t B = ctx->B;
-  register uint32_t C = ctx->C;
-  register uint32_t D = ctx->D;
+  uint32_t A = ctx->A;
+  uint32_t B = ctx->B;
+  uint32_t C = ctx->C;
+  uint32_t D = ctx->D;
   uint32_t *cwp = correct_words;
 
 #ifdef WORDS_BIGENDIAN
@@ -241,23 +242,23 @@ void MD5_write (MD5_CONTEXT *hd, const unsigned char *inbuf, unsigned long inlen
 
 void MD5_final (unsigned char *outbuf, MD5_CONTEXT *hd)
 {
-  unsigned long t, msb, lsb;
+  uint32_t t, msb, lsb;
   unsigned char *p;
 
   MD5_write(hd, NULL, 0); /* flush */
 
-  t = hd->nblocks;
+  t   = hd->nblocks;
   /* multiply by 64 to make a byte count */
   lsb = t << 6;
   msb = t >> 26;
   /* add the count */
-  t = lsb;
+  t   = lsb;
   if ((lsb += hd->count) < t) msb++;
   /* multiply by 8 to make a bit count */
-  t = lsb;
+  t     = lsb;
   lsb <<= 3;
   msb <<= 3;
-  msb |= t >> 29;
+  msb  |= t >> 29;
 
   if (hd->count < 56) { /* enough room */
     hd->buf[hd->count++] = 0x80; /* pad */
@@ -291,6 +292,320 @@ void MD5_final (unsigned char *outbuf, MD5_CONTEXT *hd)
   X(B);
   X(C);
   X(D);
+#undef X
+}
+
+/* 
+ * The following codes for the SHA256 has function were taken from
+ * libgrypt-1.6.3. (slightly modified)
+ *
+ * sha256.c - SHA256 hash function
+ * Copyright (C) 2003, 2006, 2008, 2009 Free Software Foundation, Inc.
+ *
+ * This file is part of Libgcrypt.
+ *
+ * Libgcrypt is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * Libgcrypt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+static u32
+_gcry_bswap32(u32 x)
+{
+  return ((rol(x, 8) & 0x00ff00ffL) | (ror(x, 8) & 0xff00ff00L));
+}
+
+/* Endian dependent byte swap operations.  */
+#ifdef WORDS_BIGENDIAN
+# define le_bswap32(x) _gcry_bswap32(x)
+# define be_bswap32(x) ((u32)(x))
+#else
+# define le_bswap32(x) ((u32)(x))
+# define be_bswap32(x) _gcry_bswap32(x)
+#endif
+
+static u32 buf_get_be32(const void *_buf)
+{
+  const byte *in = _buf;
+  return ((u32)in[0] << 24) | ((u32)in[1] << 16) | \
+         ((u32)in[2] << 8) | (u32)in[3];
+}
+
+static void buf_put_be32(void *_buf, u32 val)
+{
+  byte *out = _buf;
+  out[0] = val >> 24;
+  out[1] = val >> 16;
+  out[2] = val >> 8;
+  out[3] = val;
+}
+
+void
+SHA256_init (SHA256_CONTEXT *hd)
+{
+  hd->h0 = 0x6a09e667;
+  hd->h1 = 0xbb67ae85;
+  hd->h2 = 0x3c6ef372;
+  hd->h3 = 0xa54ff53a;
+  hd->h4 = 0x510e527f;
+  hd->h5 = 0x9b05688c;
+  hd->h6 = 0x1f83d9ab;
+  hd->h7 = 0x5be0cd19;
+
+  hd->nblocks = 0;
+  hd->count   = 0;
+}
+
+/*
+  Transform the message X which consists of 16 32-bit-words. See FIPS
+  180-2 for details.  */
+#define S0(x) (ror ((x), 7) ^ ror ((x), 18) ^ ((x) >> 3))       /* (4.6) */
+#define S1(x) (ror ((x), 17) ^ ror ((x), 19) ^ ((x) >> 10))     /* (4.7) */
+#define R(a,b,c,d,e,f,g,h,k,w) do                                 \
+          {                                                       \
+            t1 = (h) + Sum1((e)) + Cho((e),(f),(g)) + (k) + (w);  \
+            t2 = Sum0((a)) + Maj((a),(b),(c));                    \
+            h = g;                                                \
+            g = f;                                                \
+            f = e;                                                \
+            e = d + t1;                                           \
+            d = c;                                                \
+            c = b;                                                \
+            b = a;                                                \
+            a = t1 + t2;                                          \
+          } while (0)
+
+/* (4.2) same as SHA-1's F1.  */
+static inline u32
+Cho (u32 x, u32 y, u32 z)
+{
+  return (z ^ (x & (y ^ z)));
+}
+
+/* (4.3) same as SHA-1's F3 */
+static inline u32
+Maj (u32 x, u32 y, u32 z)
+{
+  return ((x & y) | (z & (x|y)));
+}
+
+/* (4.4) */
+static inline u32
+Sum0 (u32 x)
+{
+  return (ror (x, 2) ^ ror (x, 13) ^ ror (x, 22));
+}
+
+/* (4.5) */
+static inline u32
+Sum1 (u32 x)
+{
+  return (ror (x, 6) ^ ror (x, 11) ^ ror (x, 25));
+}
+
+
+static unsigned int
+_SHA256_transform (SHA256_CONTEXT *hd, const unsigned char *data)
+{
+  static const u32 K[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+  };
+
+  u32 a,b,c,d,e,f,g,h,t1,t2;
+  u32 w[64];
+  int i;
+
+  a = hd->h0;
+  b = hd->h1;
+  c = hd->h2;
+  d = hd->h3;
+  e = hd->h4;
+  f = hd->h5;
+  g = hd->h6;
+  h = hd->h7;
+
+  for (i=0; i < 16; i++)
+    w[i] = buf_get_be32(data + i * 4);
+  for (; i < 64; i++)
+    w[i] = S1(w[i-2]) + w[i-7] + S0(w[i-15]) + w[i-16];
+
+  for (i=0; i < 64;)
+    {
+#if 0
+      R(a,b,c,d,e,f,g,h,K[i],w[i]);
+      i++;
+#else
+      t1 = h + Sum1 (e) + Cho (e, f, g) + K[i] + w[i];
+      t2 = Sum0 (a) + Maj (a, b, c);
+      d += t1;
+      h  = t1 + t2;
+
+      t1 = g + Sum1 (d) + Cho (d, e, f) + K[i+1] + w[i+1];
+      t2 = Sum0 (h) + Maj (h, a, b);
+      c += t1;
+      g  = t1 + t2;
+
+      t1 = f + Sum1 (c) + Cho (c, d, e) + K[i+2] + w[i+2];
+      t2 = Sum0 (g) + Maj (g, h, a);
+      b += t1;
+      f  = t1 + t2;
+
+      t1 = e + Sum1 (b) + Cho (b, c, d) + K[i+3] + w[i+3];
+      t2 = Sum0 (f) + Maj (f, g, h);
+      a += t1;
+      e  = t1 + t2;
+
+      t1 = d + Sum1 (a) + Cho (a, b, c) + K[i+4] + w[i+4];
+      t2 = Sum0 (e) + Maj (e, f, g);
+      h += t1;
+      d  = t1 + t2;
+
+      t1 = c + Sum1 (h) + Cho (h, a, b) + K[i+5] + w[i+5];
+      t2 = Sum0 (d) + Maj (d, e, f);
+      g += t1;
+      c  = t1 + t2;
+
+      t1 = b + Sum1 (g) + Cho (g, h, a) + K[i+6] + w[i+6];
+      t2 = Sum0 (c) + Maj (c, d, e);
+      f += t1;
+      b  = t1 + t2;
+
+      t1 = a + Sum1 (f) + Cho (f, g, h) + K[i+7] + w[i+7];
+      t2 = Sum0 (b) + Maj (b, c, d);
+      e += t1;
+      a  = t1 + t2;
+
+      i += 8;
+#endif
+    }
+
+  hd->h0 += a;
+  hd->h1 += b;
+  hd->h2 += c;
+  hd->h3 += d;
+  hd->h4 += e;
+  hd->h5 += f;
+  hd->h6 += g;
+  hd->h7 += h;
+
+  return /*burn_stack*/ 74*4+32;
+}
+#undef S0
+#undef S1
+#undef R
+
+void SHA256_write (SHA256_CONTEXT *hd, const unsigned char *inbuf, unsigned long inlen)
+{
+  if (hd->count == 64) { /* flush the buffer */
+    _SHA256_transform(hd, hd->buf);
+    _gcry_burn_stack(80+6*sizeof(void*));
+    hd->count = 0;
+    hd->nblocks++;
+  }
+  if (!inbuf) return;
+  if (hd->count) {
+    for (; inlen && hd->count < 64; inlen--)
+      hd->buf[hd->count++] = *inbuf++;
+    SHA256_write(hd, NULL, 0);
+    if (!inlen) return;
+  }
+  _gcry_burn_stack(80+6*sizeof(void*));
+
+  while (inlen >= 64) {
+    _SHA256_transform(hd, inbuf);
+    hd->count = 0;
+    hd->nblocks++;
+    inlen -= 64;
+    inbuf += 64;
+  }
+  for (; inlen && hd->count < 64; inlen--)
+    hd->buf[hd->count++] = *inbuf++;
+}
+
+/*
+   The routine finally terminates the computation and returns the
+   digest.  The handle is prepared for a new cycle, but adding bytes
+   to the handle will the destroy the returned buffer.  Returns: 32
+   bytes with the message the digest.  */
+void
+SHA256_final(unsigned char *outbuf, SHA256_CONTEXT *hd)
+{
+  u32            t, msb, lsb;
+  unsigned char *p;
+  unsigned int   burn;
+
+  SHA256_write(hd, NULL, 0); /* flush */;
+
+  t   = hd->nblocks;
+
+  /* multiply by 64 to make a byte count */
+  lsb = t << 6;
+  msb = t >> 26;
+  /* add the count */
+  t = lsb;
+  if ((lsb += hd->count) < t)
+    msb++;
+  /* multiply by 8 to make a bit count */
+  t     = lsb;
+  lsb <<= 3;
+  msb <<= 3;
+  msb |= t >> 29;
+
+  if (hd->count < 56)
+    { /* enough room */
+      hd->buf[hd->count++] = 0x80; /* pad */
+      while (hd->count < 56)
+        hd->buf[hd->count++] = 0;  /* pad */
+    }
+  else
+    { /* need one extra block */
+      hd->buf[hd->count++] = 0x80; /* pad character */
+      while (hd->count < 64)
+        hd->buf[hd->count++] = 0;
+      SHA256_write(hd, NULL, 0);  /* flush */;
+      memset (hd->buf, 0, 56 ); /* fill next block with zeroes */
+    }
+  /* append the 64 bit count */
+  buf_put_be32(hd->buf + 56, msb);
+  buf_put_be32(hd->buf + 60, lsb);
+  burn = _SHA256_transform (hd, hd->buf);
+  _gcry_burn_stack(burn);
+
+  p = outbuf;
+#define X(a) do { *(u32*)p = be_bswap32(hd->a); p += 4; } while(0)
+  X(h0);
+  X(h1);
+  X(h2);
+  X(h3);
+  X(h4);
+  X(h5);
+  X(h6);
+  X(h7);
 #undef X
 }
 
@@ -370,6 +685,7 @@ void ARC4_set_key (ARC4_CONTEXT *ctx, unsigned int keylen, const unsigned char *
   _gcry_burn_stack(300);
 }
 
+/* AES Support */
 
 static int  rijndaelSetupEncrypt(u32 *rk, const u8 *key, int keybits);
 static void rijndaelEncrypt(const u32 *rk, int nrounds,
@@ -382,6 +698,41 @@ static void rijndaelEncrypt(const u32 *rk, int nrounds,
 #define KEYBITS   256
 #define BLOCKSIZE 16
 
+/* We don't need padding for ECB mode in PDF encryption. */
+void
+AES_ecb_set_key (AES_CONTEXT *ctx, unsigned int keylen, const unsigned char *key)
+{
+  ctx->nrounds = rijndaelSetupEncrypt(ctx->rk, key, keylen * 8);
+}
+
+void
+AES_ecb_encrypt (AES_CONTEXT *ctx,
+                 const unsigned char *plain,  size_t  plain_len,
+                 unsigned char      **cipher, size_t *cipher_len)
+{
+  const unsigned char *inptr;
+  unsigned char *outptr;
+  size_t len;
+ 
+ *cipher_len = plain_len;
+ *cipher     = NEW(*cipher_len, unsigned char);
+
+  inptr = plain; outptr = *cipher;
+  for (len = plain_len; len >= BLOCKSIZE; len -= BLOCKSIZE) {
+    rijndaelEncrypt(ctx->rk, ctx->nrounds, inptr, outptr);
+    inptr  += BLOCKSIZE;
+    outptr += BLOCKSIZE;
+  }
+  if (len > 0) {
+    unsigned char block[BLOCKSIZE];
+
+    memset(block, 0, BLOCKSIZE); /* ZeroBytePadding here */
+    memcpy(block, inptr, len);
+    rijndaelEncrypt(ctx->rk, ctx->nrounds, block, outptr);
+    inptr  += len;
+    outptr += BLOCKSIZE;
+  }
+} 
 
 void
 AES_cbc_set_key (AES_CONTEXT *ctx, unsigned int keylen, const unsigned char *key)
@@ -395,30 +746,32 @@ AES_cbc_encrypt (AES_CONTEXT *ctx,
                  unsigned char      **cipher, size_t *cipher_len)
 {
   const unsigned char *inptr;
-  unsigned char *outptr, block[BLOCKSIZE], iv[BLOCKSIZE];
-  int i, len;
-  int padbytes = BLOCKSIZE - (plain_len % BLOCKSIZE);
+  unsigned char *outptr, block[AES_BLOCKSIZE];
+  size_t len;
+  int    i;
+  int    padbytes = AES_BLOCKSIZE - (plain_len % AES_BLOCKSIZE);
 
-  for (i = 0; i < BLOCKSIZE; i++)
-    iv[i] = rand() % 0xff;
+  for (i = 0; i < AES_BLOCKSIZE; i++)
+    ctx->iv[i] = rand() % 0xff;
 
-  *cipher_len = plain_len + 16 + padbytes; /* IV and padding bytes */
+  *cipher_len = plain_len + AES_BLOCKSIZE + padbytes; /* IV and padding bytes */
   *cipher     = NEW(*cipher_len, unsigned char);
   
   inptr = plain; outptr = *cipher;
-  memcpy(outptr, iv, BLOCKSIZE);
-  outptr += BLOCKSIZE;
-  for ( len = plain_len; len >= BLOCKSIZE; len -= BLOCKSIZE) {
-    for (i = 0; i < BLOCKSIZE; i++)
-      block[i] = inptr[i] ^ iv[i];
+  memcpy(outptr, ctx->iv, AES_BLOCKSIZE);
+  outptr += AES_BLOCKSIZE;
+  for (len = plain_len; len >= AES_BLOCKSIZE; len -= AES_BLOCKSIZE) {
+    for (i = 0; i < AES_BLOCKSIZE; i++)
+      block[i] = inptr[i] ^ ctx->iv[i];
     rijndaelEncrypt(ctx->rk, ctx->nrounds, block, outptr);
-    memcpy(iv, outptr, BLOCKSIZE);
+    memcpy(ctx->iv, outptr, BLOCKSIZE);
     inptr  += BLOCKSIZE;
     outptr += BLOCKSIZE;
   }
   for (i = 0; i < BLOCKSIZE; i++)
-    block[i] = ((i < len) ? inptr[i] : padbytes) ^ iv[i];
+    block[i] = ((i < len) ? inptr[i] : padbytes) ^ ctx->iv[i];
   rijndaelEncrypt(ctx->rk, ctx->nrounds, block, outptr);
+  memcpy(ctx->iv, outptr, AES_BLOCKSIZE);
   inptr  += BLOCKSIZE;
   outptr += BLOCKSIZE;
 }
